2fc3588702fdcc1dbefff8236c7f0145c769a044
alhanoof alsagir <106310608+alhanoofalsagir@users.noreply.github.com>
2022-07-26 22:36:03 +0300

    Update views.py

--- APIEndpoints/endpoints/views.py
+++ APIEndpoints/endpoints/views.py
@@ -5,21 +5,29 @@ from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from rest_framework.request import Request
 from datetime import date
-from random import randint
+from random import choice
+
 @api_view(['GET'])
-def CurrentDate(request:Request):
-    currentDate= "Today is " , str(date.today()) , " !"
-    res= {'date':currentDate}
-    return Response(res)
+def date(_request: Request) -> Response:
+   
+    return Response({"date": f"Today is {datetime.now().strftime('%Y-%m-%d')} !"})
 
 
 @api_view(['POST'])
-def randomNumber(request : Request):
-    min= int(request.data['min'])
-    max= int(request.data['max'])
-    
-    res = {'random': randint(min, max)}
-    if min<0 : res=  {"msg": "Not Allowed. Please provide a min that is bigger than 0"}
-
-    return Response(res)
+def random_num(request: Request) -> Response:
+    if type(request.data) is dict and sorted(list(request.data)) == ["max", "min"]:
+        if all(map(lambda num: type(num) is int, request.data.values())):
+            if all(map(lambda num: num > 0, request.data.values())):
+                return Response(
+                   {"Random": choice(range(request.data["min"], request.data["max"]))}
+                )
+            else:
+                return Response(
+                    {
+                        "msg",
+                        "Not Allowed. Please provide a min and max that is bigger than 0",
+                    },
+                    status=400,
+                )
+    return Response({"msg": "Invalid request data"}, status=400)
     
